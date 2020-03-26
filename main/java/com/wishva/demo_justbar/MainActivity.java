fe353b9980a50878edbd75f45fa0cc59cafa115d
vishva-kalhara <vishvakalhara@gmail.com>
2020-03-26 06:53:19 +0530

    Add files via upload

---
+++ main/java/com/wishva/demo_justbar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.wishva.demo_justbar;
+
+import androidx.annotation.DrawableRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.irozon.justbar.BarItem;
+import com.irozon.justbar.JustBar;
+import com.irozon.justbar.interfaces.OnBarItemClickListener;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
